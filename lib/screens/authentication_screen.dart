37ef87a231cf7ff580ab9174763fbba6a094b447
Gloria Oteri <gloriaoteri@gmail.com>
2026-04-09 14:22:34 -0400

    Added email and password text fields

--- lib/screens/authentication_screen.dart
+++ lib/screens/authentication_screen.dart
@@ -31,3 +31,49 @@ class AuthService {
 
     User? get currentUser => _auth.currentUser;
 }
+
+class AuthenticationScreen extends StatefulWidget {
+  const AuthenticationScreen({super.key});
+
+  @override
+  State<AuthenticationScreen> createState() => _AuthenticationScreenState();
+}
+
+class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Email is required';
+                if (!value.contains('@')) return 'Enter a valid email like test@gsu.com';
+                return null;
+              }
+            ),
+
+            TextFormField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              validator: (value) {
+                if (value == null || value.length < 6) return 'Password must be at least 6 characters';
+                return null;
+              }
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
